53c1d058f51fe106ba67f062514af34a5856e29b
jongin403 <jongin403@naver.com>
2023-01-17 07:32:38 +0900

    feat: 각각의 함수 추가

--- src/with-redux.js
+++ src/with-redux.js
@@ -3,22 +3,56 @@ function reducer(state, action) {
     // initial state
     return { color: "yellow" };
   }
+  let newState;
+  if (action.type === "CHANGE_COLOR") {
+    newState = Object.assign({}, state, { color: action.color });
+  }
+  return newState;
 }
+
 const store = Redux.createStore(reducer);
-console.log(store.getState());
 
 function red() {
   const state = store.getState();
   document.querySelector("#red").innerHTML = `
-    <div clas="container" id="component_red" style="background-color: ${state.color}">
+    <div class="container" id="component_red" style="background-color: ${state.color}">
       <h1>red</h1>
       <input type="button" value="fire" onclick="
-        document.querySelector('#component_red').style.backgroundColor = 'red';
-        document.querySelector('#component_green').style.backgroundColor = 'red';
-        document.querySelector('#component_blue').style.backgroundColor = 'red';
+        store.dispatch({ type:'CHANGE_COLOR', color: 'red' })
       ">
     </div>
   `;
 }
 
+store.subscribe(red);
 red();
+
+function green() {
+  const state = store.getState();
+  document.querySelector("#green").innerHTML = `
+    <div class="container" id="component_green" style="background-color: ${state.color}">
+      <h1>green</h1>
+      <input type="button" value="fire" onclick="
+        store.dispatch({ type:'CHANGE_COLOR', color: 'green' })
+      ">
+    </div>
+  `;
+}
+
+store.subscribe(green);
+green();
+
+function blue() {
+  const state = store.getState();
+  document.querySelector("#blue").innerHTML = `
+    <div class="container" id="component_blue" style="background-color: ${state.color}">
+      <h1>blue</h1>
+      <input type="button" value="fire" onclick="
+        store.dispatch({ type:'CHANGE_COLOR', color: 'blue' })
+      ">
+    </div>
+  `;
+}
+
+store.subscribe(blue);
+blue();
